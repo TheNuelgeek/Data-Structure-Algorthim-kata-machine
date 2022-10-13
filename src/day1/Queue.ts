c71d01371242dc2af91f7a3449125c36dcf63ebf
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-10-13 19:22:53 +0100

    Queue implemented

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,46 @@
+type Nodee<T> = {
+    value: T,
+    next?: Nodee<T>,
+}
 export default class Queue<T> {
     public length: number;
-
+    private head?: Nodee<T>;
+    private tail?: Nodee<T>;
     
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = {value: item} as Nodee<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head =  node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
     deque(): T | undefined {
+        if (!this.head){
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        if (this.length === 0){
+            this.tail = undefined;
+        }
+        return head.value;
+
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
