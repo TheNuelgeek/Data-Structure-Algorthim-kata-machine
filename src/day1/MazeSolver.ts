1bd4e18e24e570492530052246b000885e594737
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-10-19 17:55:05 +0100

    Recursion

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,28 @@
+function walk(maze: string[], wall: string, curr: Point, end: Point, seen: boolean[][]): boolean {
+    
+    // Base Case; Off the map
+    if(curr.x < 0 || curr.x >= maze[0].length ||
+        curr.y < 0 || curr.y >= maze.length){
+
+        return false;
+    }
+
+    // On a wall
+    if(maze[curr.y][curr.x] === wall){
+        return false;
+    }
+
+    // FOund the end
+    if (curr.x === end.x && curr.y === end.y){
+        return true
+    }
+
+    // Seen
+    if (seen[curr.y][curr.x]){
+        return false
+    }
+}
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
 
-}
+
+}  
