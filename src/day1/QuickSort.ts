fbc8a64ecd00bc2cddb66c1cccf746469acba2e4
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-10-28 11:21:24 +0100

    Qick sort loading

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,21 @@
+function qs(arr: number[], lo: number, hi: number): void {
+
+}
+
+function partition(arr: number[], lo: number, hi: number): number{
+    const pviot = arr[hi];
+
+    let idx = lo - 1;
+    
+    for (let i = lo; i < hi; ++i){
+        if (arr[i] <= pviot){
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+}
 export default function quick_sort(arr: number[]): void {
 
 }
