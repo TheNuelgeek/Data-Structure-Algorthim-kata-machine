ed10c99f934738cacf922b5bac1a5b61eab36e92
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-10-28 11:28:55 +0100

    Quick sort done

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,5 +1,12 @@
 function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi){
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIdx -1);
+    qs(arr, pivotIdx + 1, hi)
 }
 
 function partition(arr: number[], lo: number, hi: number): number{
@@ -15,7 +22,13 @@ function partition(arr: number[], lo: number, hi: number): number{
             arr[idx] = tmp;
         }
     }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pviot;
+
+    return idx;
 }
 export default function quick_sort(arr: number[]): void {
-
+    qs(arr, 0, arr.length -1);
 }
