8817379fb5f375a794ad310214810861376ae6ce
TheNuelgeek <thenuelgeekmain@gmail.com>
2022-11-18 07:21:23 +0100

    Comparing binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+    // Structurl check
+    if (a === null && b === null){
+        return true;
+    }
+
+    // Structurl check
+    if (a === null || b === null){
+        return false;
+    }
+
+    // VAlue check
+    if (a.value !== b.value){
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right)
 }
